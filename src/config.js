cd99f40cc8994b9d84717e80c778895c517a7ea5
Ahmed Raihan <ahmedraihanalif@gmail.com>
2024-12-02 18:24:01 +0600

    Change login system

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    basename: '/free',
+    // basename: '/free',
     defaultPath: '/pages/login/login3', // Set login page as the default
     fontFamily: `'Roboto', sans-serif`,
     borderRadius: 12
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,7 +6,7 @@ import MainRoutes from './MainRoutes';
 
 // ==============================|| ROUTING RENDER ||============================== //
 const router = createBrowserRouter([MainRoutes, LoginRoutes], {
-  basename: import.meta.env.VITE_APP_BASE_NAME
+  // basename: import.meta.env.VITE_APP_BASE_NAME
 });
 
 export default router;

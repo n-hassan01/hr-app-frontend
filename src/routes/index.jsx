da4f7411a2baae8eb139c84dfcf24169598ca517
n-hassan01 <naimul.hassan.se@gmail.com>
2024-12-03 18:08:00 +0600

    code clean up

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,8 +5,8 @@ import LoginRoutes from './AuthenticationRoutes';
 import MainRoutes from './MainRoutes';
 
 // ==============================|| ROUTING RENDER ||============================== //
-const router = createBrowserRouter([MainRoutes, LoginRoutes], {
-  basename: import.meta.env.VITE_APP_BASE_NAME
+const router = createBrowserRouter([LoginRoutes, MainRoutes], {
+  // basename: import.meta.env.VITE_APP_BASE_NAME
 });
 
 export default router;

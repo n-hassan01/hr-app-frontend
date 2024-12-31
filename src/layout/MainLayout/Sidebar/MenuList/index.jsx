51a0997d0866f86a4180a9f6709e71040c02d49c
n-hassan01 <naimul.hassan.se@gmail.com>
2024-12-31 13:21:00 +0600

    bug fixing: rolewise menu assignment

--- src/layout/MainLayout/Sidebar/MenuList/index.jsx
+++ src/layout/MainLayout/Sidebar/MenuList/index.jsx
@@ -1,14 +1,36 @@
+import { useEffect, useState } from 'react';
+import { getUserData } from '../../../../context/UserContext';
 // material-ui
 import { Typography } from '@mui/material';
 
 // project imports
-import menuItem from 'menu-items';
 import NavGroup from './NavGroup';
+
+// menus imports dynamically
+import { dashboard, hr, interviewer } from '../../../../menu-items';
 
 // ==============================|| SIDEBAR MENU LIST ||============================== //
 
 const MenuList = () => {
-  const navItems = menuItem.items.map((item) => {
+  const user = getUserData();
+  const roles = user?.role ?? [];
+
+  const [menuItems, setMenuItems] = useState([]);
+
+  useEffect(() => {
+    const dynamicMenus = [dashboard];
+
+    if (roles.includes('INTERVIEWER')) {
+      dynamicMenus.push(interviewer);
+    }
+    if (roles.includes('HR')) {
+      dynamicMenus.push(hr);
+    }
+
+    setMenuItems(dynamicMenus);
+  }, [roles]);
+
+  const navItems = menuItems.map((item) => {
     switch (item.type) {
       case 'group':
         return <NavGroup key={item.id} item={item} />;
